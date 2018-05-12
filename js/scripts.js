06ded1acec0315c0b25e0f8c5d9fb79a7694bc7a
Your Name <you@example.com>
2018-05-12 13:38:02 +0300

    background image added under .container

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+//business interface logic//
+function play(player 1,player 2,results){
+  this.player 1=player 1;
+  this.player 2=player 2;
+  this.results=results;
+
+}
+var results,player;
+ var btnRoll=document.querySelector("btnRoll");
+  var btnHold=document.querySelector("btnHold")
+  condition()
+   for (var i = 0; i < array.length; i++) {
+     array[i]
+   }
+
+
+
+  play.prototype.results=function(){
+    return this.player 1 + ""+this.player 2
+     if (pig dice game){
+       var pig dice =Math.floor(Math.random()*6) +1;
+         var results=document.querySelector('.pig dice');
+          var results=document.querySelector()
+        if(results[player]>=50{
+          $("# ")
+
+
+
+
+
+    }
+
+     //user interface logic//
+     $(document).ready(function(){
+       $("button.btnRoll".submit(function(event){
+         $(".result").text("");
+         condition();
+         $(".result").show();
+         $("#result").val('');
+         event.preventDefault();
+       }
+     }
+
+
+
+
+
+   }
+
+}
