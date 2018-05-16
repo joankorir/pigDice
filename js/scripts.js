ef453c701060582c52a52813a27996f2f0bed895
Your Name <you@example.com>
2018-05-16 11:16:35 +0300

    modified changes under js and html

--- js/scripts.js
+++ js/scripts.js
@@ -1,84 +1,11 @@
-//business interface logic//
-
-
-//created of the players consist initialized to 0//
-function play(player 1,player 2){
-  this.player 1=name;
-  this.player 2=name;
-
+function RollDice(){
+  var dice1=document.getElementById("scores");
+  var diceroll=Math.floor(Math.random()*6)+1;
+   dice1.innerHTML=diceroll;
 
 }
- //initialized scoresPoint be 0//
-function playerturns(scoresPoint){
-    this.scoresPoint=0;
-
-
-  }
-    playerturns.prototype.scoresPoint = function () {
-    return scoresPoint(0);
-
-
-  };
-  //random number
-   if(playingturns){
-       var RollDice = math.floor(math.random()*6+1;
-       //results
-      $("#Roll").on ('click',function(){
-        return("Dice rolled");
-      };
-    };
-    //displays the current playing and limits scoresPoint
-    if(player1.activeplayer){
-       var scoresPoint =player1.Roll Dice();
-       if(score<=1){
-      player1.Hold();
-         alert("Your chance of playing is over you have scored 1")
-
-      player2.curentactiveplayer ();
-      //results
-      $("#play").val(player1.scoresPoint());
-    }
-    //The next player details
-  }  else{
-      var scoresPoint=player2.Roll Dice();
-        if(score <=1){
-      player2.Hold();
-       alert("Its the next person chance since you scored 1")
-        player1.currentplayer ();
-      $("#play-").val(player2.scoresPoint());
-
-
-  }
-    }
-
-     // displayes scores underwent that displays a winner
-    var winner=diceGameConfig.declarewinningPlayer(player1,player2);
-    if(score>=100)
-     alert("You have won the game u are The Winner")
-
-  {
-
-  }
-
-
-//user interface logic
-
-
-
-
-
-
-
-
-
-
-
-
-
-    }
-
-     //user interface logic//
-
-
+function RollDice1(){
+   var dice2=document.getElementById("scores1");
+   var dicerolled=Math.floor(Math.random()*6)+1;
+   dice2.innerHTML=dicerolled;
 }
-=-[[  ]]
